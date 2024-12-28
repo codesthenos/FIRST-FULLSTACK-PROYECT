3bf0772fdb3dd548658173ccde33be2064064afe
codesthenos <codesthenos@gmail.com>
2024-12-28 20:24:49 +0100

    Dejo una primera version del controlador que devuelve todos los anuncios filtrados, todavia no filtra nada, y dejo lo que creo que sera ya la version definitiva del controlador que devuelve un producto, revisare estas primeras versiones despues de crear y usar los middlewares

--- BACKEND-API-REST/controllers/addsController.js
+++ BACKEND-API-REST/controllers/addsController.js
@@ -1,8 +1,19 @@
+import createHttpError from 'http-errors'
 import { Add } from '../models/addModel.js'
 
-export const getAddsController = (req, res, next) => {
+export const getAddsController = async (req, res, next) => {
   // TODO
-  res.send('getAddsController')
+  try {
+    // queryParams
+    // userId from jwTokenMiddleware
+    const [adds, totalAdds] = await Promise.all([
+      Add.find(),
+      Add.countDocuments()
+    ])
+    res.json({ adds, totalAdds })
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const createAddController = (req, res, next) => {
@@ -10,9 +21,22 @@ export const createAddController = (req, res, next) => {
   res.send('createAddController')
 }
 
-export const getAddController = (req, res, next) => {
-  // TODO
-  res.send('getAddController')
+export const getAddController = async (req, res, next) => {
+  try {
+    const { id } = req.params
+
+    const add = await Add.findById(id)
+
+    if (!add) {
+      const error = createHttpError(404, 'Add not found')
+      next(error)
+      return
+    }
+
+    res.json({ add })
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const updateAddController = (req, res, next) => {
