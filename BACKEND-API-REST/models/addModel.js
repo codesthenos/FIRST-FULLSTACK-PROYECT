e3bdb2d962c32181fb9d216c5b9060ae667547d9
codesthenos <codesthenos@gmail.com>
2024-12-28 15:46:47 +0100

    Creo el schema de mongoose de Add aunque aun no he creado el metodo estatico para devolver una busqueda filtrada y ordenada

--- BACKEND-API-REST/models/addModel.js
+++ BACKEND-API-REST/models/addModel.js
@@ -1,5 +1,48 @@
 import mongoose from 'mongoose'
 
 const addSchema = new mongoose.Schema({
-  // TODO Look add schema in frontend practice REMEMBER INDEXES
-})
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true
+  },
+  price: {
+    type: Number,
+    min: 0,
+    required: true,
+    index: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true
+  },
+  for: {
+    type: String,
+    enum: ['offer', 'demand'],
+    required: true,
+    index: true
+  },
+  tags: {
+    type: [String],
+    required: true,
+    index: true
+  },
+  image: {
+    type: String,
+    required: true,
+    index: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    index: true
+  }
+}, { timestamps: true })
+
+// TODO create the static method to filter, sort etc
+
+export const Add = mongoose.model('Add', addSchema)
