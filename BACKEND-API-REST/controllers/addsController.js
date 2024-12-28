141bead48015bb282ac528d3555492755defd1af
codesthenos <codesthenos@gmail.com>
2024-12-28 22:00:44 +0100

    primeras versiones del create y update add, ahora voy a empezar a meter los middlewares empezando por el del multer para el upload y poder testear crear y editar anuncios con postman

--- BACKEND-API-REST/controllers/addsController.js
+++ BACKEND-API-REST/controllers/addsController.js
@@ -16,9 +16,40 @@ export const getAddsController = async (req, res, next) => {
   }
 }
 
-export const createAddController = (req, res, next) => {
-  // TODO
-  res.send('createAddController')
+export const createAddController = async (req, res, next) => {
+  try {
+    const {
+      name,
+      price,
+      description,
+      for: forValue,
+      tags
+    } = req.body
+
+    const userId = req.userId
+
+    const image = req.file ? `/addsImages/${req.file.filename}` : '/addsImages/placeholder.jpg'
+
+    const formattedPrice = parseFloat(price)
+
+    const tagsArray = tags.split(',').map(tag => tag.trim())
+
+    const newAdd = new Add({
+      name,
+      price: formattedPrice,
+      description,
+      for: forValue,
+      tags: tagsArray,
+      image,
+      owner: userId
+    })
+
+    const savedAdd = await newAdd.save()
+
+    res.status(201).json({ message: 'Add created', add: savedAdd })
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const getAddController = async (req, res, next) => {
@@ -39,9 +70,34 @@ export const getAddController = async (req, res, next) => {
   }
 }
 
-export const updateAddController = (req, res, next) => {
-  // TODO
-  res.send('updateAddController')
+export const updateAddController = async (req, res, next) => {
+  try {
+    const { id } = req.params
+
+    const body = req.body
+    const image = req.file ? `/addsImages/${req.file.filename}` : null
+
+    const addData = {}
+
+    if (body.name) addData.name = body.name
+    if (body.price) addData.price = parseFloat(body.price)
+    if (body.description) addData.description = body.description
+    if (body.for) addData.for = body.for
+    if (body.tags) addData.tags = body.tags.split(',').map(tag => tag.trim())
+    if (image) addData.image = image
+
+    const updatedAdd = await Add.findByIdAndUpdate(id, addData, { new: true })
+
+    if (!updatedAdd) {
+      const error = createHttpError(404, 'Add not found')
+      next(error)
+      return
+    }
+
+    res.json({ message: 'Add updated', add: updatedAdd })
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const deleteAddController = async (req, res, next) => {
